a1a4dfeccb240ecea86f566860e66cf02d1cf08d
yazan-hamamdi <120384088+yazan-hamamdi@users.noreply.github.com>
2023-07-28 16:03:46 +0300

    create 4 new methods 1.Edits the list of products 2. Delete product 3. Search for a product 4 close the system

--- Inventory Management System/Product.cs
+++ Inventory Management System/Product.cs
@@ -5,13 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Inventory_Management_System
-{     
+{
     internal class Product
     {
         private string _name;
         private double _price;
         private double _quantity;
-        public Product( string name, double price, double quantity)
+        public Product(string name, double price, double quantity)
         {
             this._name = name;
             this._price = price;
@@ -20,13 +20,13 @@ namespace Inventory_Management_System
         private double check(double val) => val <= 0 ? 0 : val;
         public string name
         {
-            set { this._name =value;}
+            set { this._name = value; }
 
-            get{ return this._name;}
+            get { return this._name; }
         }
         public double price
         {
-            set { this._price =check(value); }
+            set { this._price = check(value); }
 
             get { return this._price; }
         }
@@ -40,12 +40,12 @@ namespace Inventory_Management_System
         //*Add a product:* Prompt the user for the product name, price, and quantity, then add the product to the inventory
         public Product AddProduct(string name, double price, double quantity)
         {
-          Product p=new Product(name, price, quantity);
+            Product p = new Product(name, price, quantity);
             return p;
         }
 
         //*View all products:* Display a list of all products in the inventory
-        public void DisplayProducts(List<Product>L)
+        public void DisplayProducts(List<Product> L)
         {
             foreach (var pro in L)
             {
@@ -55,5 +55,44 @@ namespace Inventory_Management_System
                 Console.WriteLine("+++++++++++++++++++++");
             }
         }
+
+        //Edit a product:* Ask the user for a product name. If the product is in the inventory, allow the user to update its name, price, or quantity.
+        public void EditProducts(List<Product> L, string name)
+        {
+            foreach (var pro in L)
+            {
+                if (pro.name == name)
+                    Console.Write("update your name:  ");
+                pro.name = Console.ReadLine();
+            }
+        }
+        //Delete a product:* Ask the user for a product name. If the product is in the inventory, remove it. Commit and push.
+        public void DeleteProducts(List<Product> L, string name)
+        {
+            foreach (var pro in L)
+            {
+                if (pro.name == name)
+                    L.Remove(pro);
+            }
+        }
+
+        //Search for a product:* Ask the user for a product name. If the product is in the inventory, display its name, price, and quantity. If not, let the user know the product was not found
+        public void SearchProducts(List<Product> L, string name)
+        {
+            foreach (var pro in L)
+            {
+                if (pro.name == name)
+                {
+                    Console.WriteLine($"name is: {pro.name}");
+                    Console.WriteLine($"price is: {pro.price}");
+                    Console.WriteLine($"name is: {pro.quantity}");
+                }
+                else
+                    Console.WriteLine("the product was not found");
+            }
+        }
+        // *Exit:* Close the application
+        public bool Close()
+         { return false;}
     }
 }
